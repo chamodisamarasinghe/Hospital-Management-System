a7cca516e7c627c2f0c6db2676a6d662adf30e90
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-09-12 20:52:48 +0530

    Added images

--- src/db/DbConnection.java
+++ src/db/DbConnection.java
@@ -13,7 +13,7 @@ public class DbConnection {
         connection = DriverManager.getConnection(
                 "jdbc:mysql://127.0.0.1:3306/HospitalManagementSystem",
                 "root",
-                "1234"
+                "Chamodi@123"
         );
     }
 
